73471826376d08666d1a9d5615d43a9037c1569d
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2021-09-09 20:19:30 -0500

    better errors

--- request.js
+++ request.js
@@ -6,6 +6,7 @@ module.exports = function (hostname, prefix, authorization) {
   const store = {};
   const queue = new AsyncQueue();
   return async function (bucket, method, path, body, query, files) {
+    path = prefix + path;
     const headers = { authorization };
 
     if (body) {
@@ -34,23 +35,38 @@ module.exports = function (hostname, prefix, authorization) {
       await queue.wait();
       const ratelimit = store[bucket];
       if (ratelimit) {
-        await new Promise(resolve => setTimeout(resolve, -(Date.now() - ratelimit * 1000)));
-        store[bucket] = 0;
+        await new Promise(resolve => setTimeout(resolve, ratelimit - Date.now()));
+        delete store[bucket];
       }
 
       const req = raw(
         {
+          path,
           method,
           headers,
-          hostname,
-          path: prefix + path
+          hostname
         },
         res => {
           queue.shift();
-          if (res.headers["x-ratelimit-remaining"] === "0") store[bucket] = res.headers["x-ratelimit-reset"];
-          if (res.statusCode < 200 || res.statusCode >= 400)
-            reject(Error(method + " " + path + " returned " + res.statusCode + ": " + res.statusMessage));
-          else {
+          if (res.headers["x-ratelimit-remaining"] === "0") store[bucket] = res.headers["x-ratelimit-reset"] * 1000;
+          if (res.statusCode < 200 || res.statusCode >= 400) {
+            const err = Error(method + " " + path + " returned " + res.statusCode + ": " + res.statusMessage);
+            err.path = path;
+            err.body = body;
+            err.files = files;
+            err.query = query;
+            err.method = method;
+            err.status = res.statusCode;
+            err.statusMsg = res.statusMessage;
+
+            let text = "";
+            res.on("data", chunk => (text += chunk));
+            res.on("end", () => {
+              err.response = res.headers["content-type"] === "application/json" ? JSON.parse(text) : text;
+              err.message = err.response.message ?? err.response;
+              reject(err);
+            });
+          } else {
             let text = "";
             res.on("data", chunk => (text += chunk));
             res.on("end", () => resolve(res.headers["content-type"] === "application/json" ? JSON.parse(text) : text));
